d6d462e45a592328bb3c66b5db05e1901aeccc39
edcel.ceniza <edcel.ceniza@gmail.com>
2021-11-29 22:44:23 +1300

    added quote on input variable

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ define('CHANNEL', getenv('CHANNEL'));
  // Grab event data from the request
 //$input = $_POST['body'];
 
-$input = {"type": "url_verification","token": "sadasdasdasd","challenge": "dasdasdasdasad"};
+$input = '{"type": "url_verification","token": "sadasdasdasd","challenge": "dasdasdasdasad"}';
 $json = json_decode($input, FALSE);
 $type = $json->{type};
 
