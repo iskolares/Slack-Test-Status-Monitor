2cb62425b298a80a9d9851e6af82513ef6912ec4
edcel.ceniza <edcel.ceniza@gmail.com>
2021-11-30 02:21:56 +1300

    fixed syntax variable name

--- index.php
+++ index.php
@@ -47,9 +47,7 @@ switch ($jsontype) {
         // Grab some data about the user;
         $userid = $json->event->user->id;
 
-console.log($userid);
-
-        $getuserprofile = "https://slack.com/api/users.profile.get?user=" . "$userid" . "&pretty=1";
+        $getuserprofile = "https://slack.com/api/users.profile.get?user=".$userid;
         $curl = curl_init($getuserprofile);
         curl_setopt($curl, CURLOPT_URL, $getuserprofile);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
